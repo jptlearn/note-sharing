beaee6103d20c031a3c3d4cf8042318b644a91cd
skcvlearn404 <skcvlearn404@gmail.com>
2025-01-28 18:51:08 +0545

    update cofig for development purpose only

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,7 @@ const envConfig = {
   mongodbURI: process.env.MONGODB_URI,
   dbName: process.env.MONGO_DB_NAME,
   backendURL: process.env.BACKEND_URL,
+  nodeEnv: process.env.NODE_ENV,
 };
 
 export default envConfig;
