58857ae033042a76e24bb2e8214cdb7434505a4b
skcvlearn404 <skcvlearn404@gmail.com>
2025-01-28 18:51:31 +0545

    create global error handler file

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,1 +1,19 @@
-const globalErrorHandler = {};
+import { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import envConfig from "../config/config";
+
+const globalErrorHandler = (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    message: err.message,
+    errorStack:
+      envConfig.nodeEnv === "development" ? err.stack : "Something went wrong",
+  });
+};
+
+export default globalErrorHandler;
