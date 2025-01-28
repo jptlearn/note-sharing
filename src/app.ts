f780fa76aab534af78eda11262452acf60d29da7
skcvlearn404 <skcvlearn404@gmail.com>
2025-01-28 19:01:34 +0545

    update app file with router and handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+import noteRouter from "./note/noteRouter";
 
 const app = express();
+
+app.use(globalErrorHandler);
+
+app.use("/api/v1/notes", noteRouter);
 
 export default app;
